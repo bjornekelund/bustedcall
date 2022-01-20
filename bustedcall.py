e63885908992b0503eff92afe8529708626cb189
Björn <bjorn@ekelund.nu>
2022-01-20 13:37:07 +0100

    Update

--- bustedcall.py
+++ bustedcall.py
@@ -1,40 +1,124 @@
 #!/bin/python3
 
-from time import sleep
-from spot import Spot
+from Levenshtein import distance
 import csv
-from helpers import contestband, modeisCW, isskimmer, since
-import re
 from datetime import datetime
 
-FILE="20211123.csv"
+#FILE="20211123.csv"
+#FILE="test.csv"
+FILE="small.csv"
+MASTER="MASTER.SCP"
 
-#SIZE1 = 60 # "RBN" buffer size in seconds
-#FIFO1 = [] # "RBN" buffer
+WINDOW = 60 # RBN bust buffer size in seconds
+FIFO1 = [] # RBN buffer
+
+FREQMARGIN = 0.3 # Acceptable offset to be considered the same frequency
 
 SPOTS = [] # Spots array
+SCP = [] # Valid callsigns array
 
-def contestband(freq):
+MORSE = {
+    "A" : ".- ",
+    "B" : "-... ",
+    "C" : "-.-. ",
+    "D" : "-.. ",
+    "E" : ". ",
+    "F" : "..-. ",
+    "G" : "--. ",
+    "H" : ".... ",
+    "I" : ".. ",
+    "J" : ".--- ",
+    "K" : "-.- ",
+    "L" : ".-.. ",
+    "M" : "-- ",
+    "N" : "-. ",
+    "O" : "--- ",
+    "P" : ".--. ",
+    "Q" : "--.- ",
+    "R" : ".-. ",
+    "S" : "... ",
+    "T" : "- ",
+    "U" : "..- ",
+    "V" : "...- ",
+    "W" : ".-- ",
+    "X" : "-..- ",
+    "Y" : "-.-- ",
+    "Z" : "--.. ",
+    "0" : "----- ",
+    "1" : ".---- ",
+    "2" : "..--- ",
+    "3" : "...-- ",
+    "4" : "....- ",
+    "5" : "..... ",
+    "6" : "-.... ",
+    "7" : "--... ",
+    "8" : "---.. ",
+    "9" : "----. ",
+    "/" : "-..-. ",
+}
+
+def contestband(freqstring):
     bands = [(1800, 2000), (3500, 3800), (7000, 7300), (14000, 14350), (21000, 21450), (28000, 29700)]
+    freq = float(freqstring)
     for (lower, upper) in bands:
         if freq >= lower and freq <= upper:
             return True
     return False
 
-def modeisCW(line):
-    if (re.match(".+ CW ", line)):
-        return True
+def morse(callsign):
+    result = ""
+    for char in callsign:
+        result += MORSE[char]
+    return result[0:-1] # Prune last space
+
+def levenshtein(validspot, checkspot, freqmargin):
+    if abs(validspot.qrg - checkspot.qrg) <= freqmargin:
+        dist = distance(validspot.morse, checkspot.morse)
+        # dist = distance(validspot.dx, checkspot.dx)
+        result = dist
+        # print(f'Reference {validspot.dx}@{validspot.qrg} and {checkspot.dx}@{checkspot.qrg} distance is {dist}')
     else:
-        return False
+        result = 99
+    return result
 
-def since(time):
-    return round((datetime.utcnow() - time).total_seconds(), 0)
+class Spot():
+    def __init__(self, spotter, call, frequency, date):
+        self.origin = spotter
+        self.qrg = float(frequency)
+        self.dx = call
+        self.time = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
+        self.morse = morse(call)
+        self.valid = call in SCP       
+        self.exposed = False
+        # print("Call %8s QRG %5.1f Morse \"%s\"" % (self.dx, self.qrg, self.morse))           
+       
+# Load the MASTER.SCP database in global array SCP
 
+#calls = []
+call_count = 0
+with open(MASTER) as f:
+    calls = f.read().splitlines()
+    for call in calls:
+        if not call.startswith("#"):
+            # print(f'Added line "{call}"')
+            SCP.append(call)
+            call_count += 1          
+f.close()
+print(f'Processed {call_count} calls.')
+
+# Load all spots in global array SPOTS
+
+spot_count = -1
+valid_count = 0
+ispotter = 0
+idx = 0
+idate =0
+ifreq = 0
+imode = 0
 with open(FILE) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
     for row in csv_reader:
-        if line_count == 0:
+        if spot_count == -1:
             for i in range(0, len(row)):
                 if row[i] == "callsign": 
                     ispotter = i
@@ -44,65 +128,37 @@ with open(FILE) as csv_file:
                     idate = i
                 if row[i] == "freq":
                     ifreq = i
-
-            print(f'Spotter is {ispotter}, dx is {idx}, freq is {ifreq}, and date is {idate}')
-            line_count += 1
+                if row[i] == "tx_mode":
+                    imode = i
+            # print(f'Spotter is {ispotter}, dx is {idx}, freq is {ifreq}, and date is {idate}')
+            spot_count = 0
         else:
-# Process line
-            if len(row) >= idate: 
-                SPOTS.append(Spot(row[ispotter], row[idx], row[ifreq], row[idate]))
-                # print(f'Processig spot #{line_count}')
-                line_count += 1
-    print(f'Processed {line_count} lines.')
+            if len(row) >= idate and contestband(row[ifreq]) and row[imode] == 'CW':
+                newspot = Spot(row[ispotter], row[idx], row[ifreq], row[idate])
+                if newspot.valid:
+                    valid_count += 1
+                SPOTS.append(newspot)
+                spot_count += 1
+                if spot_count % 10000 == 0:
+                    print(f'Building spot array, spot #{spot_count}')
+csv_file.close()
+print(f'Processed {spot_count} spots of which {valid_count} validated.')
     
-    # for i in range(55, 62):
-        # print(f'orig={SPOTS[i].origin} dx={SPOTS[i].dx} fq={SPOTS[i].qrg} date={SPOTS[i].time}')
+# Now traverse the entire spot array and find busted calls
+# A busted call is roughly on the same frequency and has a small enough difference to a valid call within the time window
 
-#if __name__ == '__main__':
-#    tw9pa = telnetlib.Telnet('dxc.w9pa.net', 7373, 5)
-#
-#    node = 'W9PA-4'
-#
-#    alerted = False
-#    ignored = tw9pa.read_until(b'your call:').decode('latin-1')
-#    sleep(1)
-#    tw9pa.write(MYCALL.encode('ascii') + b'\n')
-#    tw9pa.write(b'set dx extension skimmerquality\n')
-#    fifo1filled = False
-#    displayedtime = 0
-#
-#    while True:
-#        try:
-#            line = tw9pa.read_until(b'\n').decode('latin-1')
-#            if line.upper().startswith('DX DE ') and modeisCW(line) and isskimmer(line):
-#                # Add incoming spot to end of queue
-#                FIFO1.append(Spot(line, node))
-#                # While oldest spot is older than SIZE1, keep processing
-#                while since(FIFO1[0].timestamp) >= SIZE1:
-#                    fifo1filled = True
-#                    oldspot = FIFO1.pop(0) # Remove oldest spot from queue
-#                    # Only process ? spots that are on contest bands
-#                    if contestband(oldspot.qrg) and oldspot.quality == '?':
-#                        found = False
-#                        for w9paspot in FIFO1:
-#                            if oldspot.callsign == w9paspot.callsign and \
-#                                    abs(oldspot.qrg - w9paspot.qrg) < 0.4 and w9paspot.quality == 'V':
-#                                found = True
-#                                delay = round((w9paspot.timestamp - oldspot.timestamp).total_seconds(), 1)
-#                                break
-#                        if not found:
-#                            print('%s: ? spot still not V after %3ds  ==> %s' % (node, SIZE1, oldspot.toString()))
-#                        else:
-#                            print('%s: ? spot became V after %4.1fs    ==> %s' % (node, delay, oldspot.toString()))
-#                if not fifo1filled:
-#                    timeleft = int(SIZE1 - since(FIFO1[0].timestamp))
-#                    if timeleft % 10 == 0 and timeleft != 0 and timeleft != displayedtime:
-#                        print(f'Filling pipeline, analysis will start in %ds...' % timeleft)
-#                        displayedtime = timeleft
-#                if not alerted:
-#                    print(node + " feed active")
-#                    alerted = True
-#        except KeyboardInterrupt:
-#            tw9pa.close()
-#            print('\nDisconnected')
-#            exit(0)
+for newspot in SPOTS:
+    FIFO1.append(newspot)
+    # Process all spots at the oldest end of the FIFO that about to expire
+    while (FIFO1[-1].time - FIFO1[0].time).total_seconds() > WINDOW:
+        spot = FIFO1.pop(0)
+        if spot.valid: # If it is a known callsign
+            for check in FIFO1: # Check for "bad copies" in the FIFO
+                if not check.valid and not check.exposed:
+                    check.exposed = True # Don't display a bad spot more than once
+                    tdelta = (check.time - spot.time).total_seconds()
+                    fdelta = abs(check.qrg - spot.qrg)
+                    dist = levenshtein(spot, check, FREQMARGIN)
+                    if dist <= 5:
+                        print("Busted spot %8s %.0f seconds apart and %.1f kHz apart (should be %s distance is %d)" % (check.dx, tdelta, fdelta, spot.dx, dist))
+            
--- getdata.bash
+++ getdata.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-DATES="20211123 20211127 20211128"
+DATES="20211123 20211127 20211128 20220111"
 for date in $DATES; do
   ZIP="$date.gz"
   CSV="$date.csv"
@@ -11,5 +11,9 @@ for date in $DATES; do
   mv $date $CSV
   echo "Done"
 done
+echo "Downloading MASTER.SCP"
+rm -f MASTER.SCP
+wget --quiet --no-hsts http://www.supercheckpartial.com/MASTER.SCP
+echo "Done"
 exit
 
