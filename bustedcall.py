7713be20f348a6a5bf4c95e7c8f3bfe707edccb5
Björn <bjorn@ekelund.nu>
2022-01-20 15:23:20 +0100

    Update

--- bustedcall.py
+++ bustedcall.py
@@ -3,13 +3,18 @@
 from Levenshtein import distance
 import csv
 from datetime import datetime
+import sys
 
 #FILE="20211123.csv"
-#FILE="test.csv"
+# FILE="test.csv"
 FILE="small.csv"
 MASTER="MASTER.SCP"
 
-WINDOW = 60 # RBN bust buffer size in seconds
+USEMORSE = True
+MORSEMAXDIST = 5
+ASCIIMAXDIST = 3
+
+WINDOW = 15 # RBN bust buffer size in seconds
 FIFO1 = [] # RBN buffer
 
 FREQMARGIN = 0.3 # Acceptable offset to be considered the same frequency
@@ -73,7 +78,10 @@ def morse(callsign):
 
 def levenshtein(validspot, checkspot, freqmargin):
     if abs(validspot.qrg - checkspot.qrg) <= freqmargin:
-        dist = distance(validspot.morse, checkspot.morse)
+        if USEMORSE:
+            dist = distance(validspot.morse, checkspot.morse)
+        else:
+            dist = distance(validspot.dx, checkspot.dx)
         # dist = distance(validspot.dx, checkspot.dx)
         result = dist
         # print(f'Reference {validspot.dx}@{validspot.qrg} and {checkspot.dx}@{checkspot.qrg} distance is {dist}')
@@ -89,76 +97,93 @@ class Spot():
         self.time = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
         self.morse = morse(call)
         self.valid = call in SCP       
-        self.exposed = False
-        # print("Call %8s QRG %5.1f Morse \"%s\"" % (self.dx, self.qrg, self.morse))           
+        self.exposed = "/" in call
+        # if not self.valid:
+            # print("Call %8s QRG %5.1f Morse \"%s\"" % (self.dx, self.qrg, self.morse))           
        
 # Load the MASTER.SCP database in global array SCP
 
-#calls = []
-call_count = 0
-with open(MASTER) as f:
-    calls = f.read().splitlines()
-    for call in calls:
-        if not call.startswith("#"):
-            # print(f'Added line "{call}"')
-            SCP.append(call)
-            call_count += 1          
-f.close()
-print(f'Processed {call_count} calls.')
+if __name__ == "__main__":
 
-# Load all spots in global array SPOTS
-
-spot_count = -1
-valid_count = 0
-ispotter = 0
-idx = 0
-idate =0
-ifreq = 0
-imode = 0
-with open(FILE) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    for row in csv_reader:
-        if spot_count == -1:
-            for i in range(0, len(row)):
-                if row[i] == "callsign": 
-                    ispotter = i
-                if row[i] == "dx":
-                    idx = i
-                if row[i] == "date":
-                    idate = i
-                if row[i] == "freq":
-                    ifreq = i
-                if row[i] == "tx_mode":
-                    imode = i
-            # print(f'Spotter is {ispotter}, dx is {idx}, freq is {ifreq}, and date is {idate}')
-            spot_count = 0
+    for i in range(1, len(sys.argv)):
+        if sys.argv[i] == "-a":
+            USEMORSE = False
         else:
-            if len(row) >= idate and contestband(row[ifreq]) and row[imode] == 'CW':
-                newspot = Spot(row[ispotter], row[idx], row[ifreq], row[idate])
-                if newspot.valid:
-                    valid_count += 1
-                SPOTS.append(newspot)
-                spot_count += 1
-                if spot_count % 10000 == 0:
-                    print(f'Building spot array, spot #{spot_count}')
-csv_file.close()
-print(f'Processed {spot_count} spots of which {valid_count} validated.')
-    
-# Now traverse the entire spot array and find busted calls
-# A busted call is roughly on the same frequency and has a small enough difference to a valid call within the time window
+            FILE = sys.argv[i]
 
-for newspot in SPOTS:
-    FIFO1.append(newspot)
-    # Process all spots at the oldest end of the FIFO that about to expire
-    while (FIFO1[-1].time - FIFO1[0].time).total_seconds() > WINDOW:
-        spot = FIFO1.pop(0)
-        if spot.valid: # If it is a known callsign
-            for check in FIFO1: # Check for "bad copies" in the FIFO
-                if not check.valid and not check.exposed:
-                    check.exposed = True # Don't display a bad spot more than once
-                    tdelta = (check.time - spot.time).total_seconds()
-                    fdelta = abs(check.qrg - spot.qrg)
-                    dist = levenshtein(spot, check, FREQMARGIN)
-                    if dist <= 5:
-                        print("Busted spot %8s %.0f seconds apart and %.1f kHz apart (should be %s distance is %d)" % (check.dx, tdelta, fdelta, spot.dx, dist))
-            
+    call_count = 0
+    with open(MASTER) as f:
+        calls = f.read().splitlines()
+        for call in calls:
+            if not call.startswith("#"):
+                # print(f'Added line "{call}"')
+                SCP.append(call)
+                call_count += 1          
+    f.close()
+    print(f'Loaded {call_count} validated calls.')
+
+    # Load all spots in global array SPOTS
+
+    spot_count = -1
+    valid_count = 0
+    ispotter = 0
+    idx = 0
+    idate =0
+    ifreq = 0
+    imode = 0
+    print("Reading file %s..." % FILE)
+    with open(FILE) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            if spot_count == -1:
+                for i in range(0, len(row)):
+                    if row[i] == "callsign": 
+                        ispotter = i
+                    if row[i] == "dx":
+                        idx = i
+                    if row[i] == "date":
+                        idate = i
+                    if row[i] == "freq":
+                        ifreq = i
+                    if row[i] == "tx_mode":
+                        imode = i
+                # print(f'Spotter is {ispotter}, dx is {idx}, freq is {ifreq}, and date is {idate}')
+                spot_count = 0
+            else:
+                if len(row) >= idate and contestband(row[ifreq]) and row[imode] == 'CW':
+                # if len(row) >= idate and row[imode] == 'CW':
+                    newspot = Spot(row[ispotter], row[idx], row[ifreq], row[idate])
+                    if newspot.valid:
+                        valid_count += 1
+                    SPOTS.append(newspot)
+                    spot_count += 1
+                    if spot_count % 10000 == 0:
+                        print(f'Building spot array, spot #{spot_count}')
+    csv_file.close()
+    print(f'Processed {spot_count} spots of which {valid_count} are of known good calls.')
+        
+    # Now traverse the entire spot array and find busted calls
+    # A busted call is roughly on the same frequency and has a small enough difference to a valid call within the time window
+
+    count_bust = 0
+    for newspot in SPOTS:
+        FIFO1.append(newspot)
+        # Process all spots at the oldest end of the FIFO that about to expire
+        while (FIFO1[-1].time - FIFO1[0].time).total_seconds() > WINDOW:
+            spot = FIFO1.pop(0)
+            if spot.valid: # If it is a known callsign      
+                for check in FIFO1: # Check for "bad copies" in the FIFO
+                    if not check.valid and not check.exposed:
+                        check.exposed = True # Don't display a bad spot more than once
+                        tdelta = (check.time - spot.time).total_seconds()
+                        fdelta = abs(check.qrg - spot.qrg)
+                        dist = levenshtein(spot, check, FREQMARGIN)
+                        # print("%s vs %s dist %d" % (spot.dx, check.dx, dist))
+                        if USEMORSE:
+                            maxdist = MORSEMAXDIST
+                        else:
+                            maxdist = ASCIIMAXDIST
+                        if dist <= maxdist:
+                            count_bust += 1
+                            print("Busted spot %8s %.0f seconds after correct call and %.1f kHz off (call is %s and distance is %d)" % (check.dx, tdelta, fdelta, spot.dx, dist))
+    print("A total of %d busted spots with %s method" % (count_bust, "Morse" if USEMORSE else "ASCII"))
