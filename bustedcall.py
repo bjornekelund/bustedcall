656f7392f744833cdd6fd6f3c31e70e0fe6c43d7
Björn <bjorn@ekelund.nu>
2022-01-21 08:30:13 +0100

    Update

--- bustedcall.py
+++ bustedcall.py
@@ -1,18 +1,17 @@
 #!/bin/python3
-
 from Levenshtein import distance
 import csv
 from datetime import datetime
 import sys
 
-#FILE="20211123.csv"
+# FILE="20211123.csv"
 # FILE="test.csv"
 FILE="small.csv"
 MASTER="MASTER.SCP"
 
 USEMORSE = True
-MORSEMAXDIST = 6
-ASCIIMAXDIST = 2
+MORSEMAXDISTS = [4, 5, 6]
+ASCIIMAXDISTS = [1, 2, 3]
 
 WINDOW = 15 # RBN bust buffer size in seconds
 FIFO1 = [] # RBN buffer
@@ -20,7 +19,7 @@ FIFO1 = [] # RBN buffer
 FREQMARGIN = 0.2 # Acceptable offset to be considered the same frequency
 
 SPOTS = [] # Spots array
-SCP = [] # Valid callsigns array
+VALIDATEDCALLS = [] # Valid callsigns array
 
 MORSE = {
     "A" : ".- ",
@@ -76,9 +75,9 @@ def morse(callsign):
         result += MORSE[char]
     return result[0:-1] # Prune last space
 
-def levenshtein(validspot, checkspot, freqmargin):
+def levenshtein(validspot, checkspot, freqmargin, metric):
     if abs(validspot.qrg - checkspot.qrg) <= freqmargin:
-        if USEMORSE:
+        if metric == "Morse":
             result = distance(validspot.morse, checkspot.morse)
         else:
             result = distance(validspot.dx, checkspot.dx)
@@ -94,39 +93,16 @@ class Spot():
         self.dx = call
         self.time = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
         self.morse = morse(call)
-        self.valid = call in SCP       
+        self.valid = call in VALIDATEDCALLS
         self.exposed = "/" in call
         # if not self.valid:
-            # print("Call %8s QRG %5.1f Morse \"%s\"" % (self.dx, self.qrg, self.morse))           
-       
+            # print("Call %8s QRG %5.1f Morse \"%s\"" % (self.dx, self.qrg, self.morse))
+
 if __name__ == "__main__":
-
-    maxdist = MORSEMAXDIST
-    ismax = False
-    maxset = False
-    onlymax = False
     for i in range(1, len(sys.argv)):
-        if ismax:
-            maxdist = int(sys.argv[i])
-            ismax = False
-        elif sys.argv[i] == "-a":
-            USEMORSE = False
-            if not maxset:
-                maxdist = ASCIIMAXDIST
-        elif sys.argv[i] == "-h" or sys.argv[i] == "-?":
-            print("Usage: ./bustedcall [-ahp] [-m maxdist] filename.csv")
-            exit(0)
-        elif sys.argv[i] == "-m":
-            ismax = True
-            maxset = True
-        elif sys.argv[i] == "-p" and not ismax:
-            onlymax = True
-        elif not ismax:
-            FILE = sys.argv[i]
+        FILE = sys.argv[i]
 
-    print("Maxdist=%d file=%s %s" % (maxdist, FILE, "Morse" if USEMORSE else "ASCII"))
-    print("Show only max distance busts" if onlymax else "Show all busts")
-    # Load the MASTER.SCP database in global array SCP
+    # Load the MASTER.SCP database in global array VALIDATEDCALLS
 
     call_count = 0
     with open(MASTER) as f:
@@ -134,8 +110,8 @@ if __name__ == "__main__":
         for call in calls:
             if not call.startswith("#"):
                 # print(f'Added line "{call}"')
-                SCP.append(call)
-                call_count += 1          
+                VALIDATEDCALLS.append(call)
+                call_count += 1
     f.close()
     print(f'Loaded {call_count} validated calls.')
 
@@ -145,7 +121,7 @@ if __name__ == "__main__":
     valid_count = 0
     ispotter = 0
     idx = 0
-    idate =0
+    idate = 0
     ifreq = 0
     imode = 0
     print("Reading file %s..." % FILE)
@@ -154,7 +130,7 @@ if __name__ == "__main__":
         for row in csv_reader:
             if spot_count == -1:
                 for i in range(0, len(row)):
-                    if row[i] == "callsign": 
+                    if row[i] == "callsign":
                         ispotter = i
                     if row[i] == "dx":
                         idx = i
@@ -179,25 +155,48 @@ if __name__ == "__main__":
                         sys.stderr.flush()
     csv_file.close()
     print(f'Processed {spot_count} spots of which {valid_count} are of known good calls.')
-        
+
     # Now traverse the entire spot array and find busted calls
     # A busted call is roughly on the same frequency and has a small enough difference to a valid call within the time window
 
-    count_bust = 0
-    for newspot in SPOTS:
-        FIFO1.append(newspot)
-        # Process all spots at the oldest end of the FIFO that about to expire
-        while (FIFO1[-1].time - FIFO1[0].time).total_seconds() > WINDOW:
-            spot = FIFO1.pop(0)
-            if spot.valid: # If it is a known callsign      
-                for check in FIFO1: # Check for "bad copies" in the FIFO
-                    if not check.valid and not check.exposed:
-                        check.exposed = True # Don't display a bad spot more than once
-                        tdelta = (check.time - spot.time).total_seconds()
-                        fdelta = abs(check.qrg - spot.qrg)
-                        dist = levenshtein(spot, check, FREQMARGIN)
-                        # print("%s vs %s dist %d" % (spot.dx, check.dx, dist))
-                        if (onlymax and (dist == maxdist)) or (not onlymax and (dist <= maxdist)):
-                            count_bust += 1
-                            print("Busted spot %7s %2.0fs after correct call and %.1f kHz off (actually %7s with %d distance)" % (check.dx, tdelta, fdelta, spot.dx, dist))
-    print("A total of %d busted spots with %s method" % (count_bust, "Morse" if USEMORSE else "ASCII"))
+    for metric in ["Morse", "ASCII"]: # For both methods
+        for showonlymax in (False, True): # Show all and only the worst
+            if metric == "Morse":
+                dists = MORSEMAXDISTS
+            else:
+                dists = ASCIIMAXDISTS
+            for maxdist in dists: # For all studied max distances
+                FIFO1 = []
+
+                # Start analysis
+
+                print("---------------")
+                print("Starting analysis for %s-based metric with maximum distance of %d" % (metric, maxdist))
+                print("SPOTS has %d entries" % len(SPOTS))
+                if showonlymax:
+                    print("Showing only maximum distance busts")
+                count_bust = 0
+
+                for newspot in SPOTS:
+                    FIFO1.append(newspot)
+                    # Process all spots at the oldest end of the FIFO that about to expire
+                    while (FIFO1[-1].time - FIFO1[0].time).total_seconds() > WINDOW:
+                        spot = FIFO1.pop(0)
+                        if spot.valid: # If it is a known callsign
+                            for check in FIFO1: # Check for "bad copies" in the FIFO
+            #                    if not check.valid and not check.exposed:
+                                if not check.exposed:
+                                    check.exposed = True # Don't display a bad spot more than once
+                                    tdelta = (check.time - spot.time).total_seconds()
+                                    fdelta = abs(check.qrg - spot.qrg)
+                                    dist = levenshtein(spot, check, FREQMARGIN, metric)
+                                    # print("%s vs %s dist %d" % (spot.dx, check.dx, dist))
+                                    if (showonlymax and (dist == maxdist)) or (not showonlymax and (dist > 0 and dist <= maxdist)):
+                                        count_bust += 1
+                                        print("Busted spot %7s %2.0fs after correct call and %.1f kHz off (actually %7s with %d distance)" % (check.dx, tdelta, fdelta, spot.dx, dist))
+                if showonlymax:
+                    print("A total of %d busted spots with %s method and a distance of %d" % (count_bust, metric, maxdist))
+                else:
+                    print("A total of %d busted spots with %s method and distance of less than %d" % (count_bust, metric, maxdist))
+                for spot in SPOTS:
+                    spot.exposed = False
